9943f4e449e07cbd4bb26b1e1dcfb5a63243d2c1
CN\ex_zhoulq6 <ex_zhoulq6@partner.midea.com>
2024-04-22 14:13:59 +0800

    修改1

--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,7 @@ export default defineComponent({
   components: { ProjectSetting },
   setup() {
     console.log('1-------')
+    console.log('2-------')
     const globalStore = useGlobalStore();
 
     const menuTree = computed(() => globalStore.menuTree);
